64a605060fc4405f5c118888af6873812309a8cc
pzkcommunity <1043647476@qq.com>
2020-02-27 17:58:11 +0800

    最终配置

--- src/test/java/com/pzk/community/CommunityApplicationTests.java
+++ src/test/java/com/pzk/community/CommunityApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CommunityApplicationTests {
+public class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
